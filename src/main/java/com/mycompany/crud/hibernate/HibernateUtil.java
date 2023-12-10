2edf1f625ecc5eb46ae24030c8d9e67c7abfbf16
Wendy Vallejo <wbvalpat24@gmail.com>
2023-12-09 20:24:00 -0600

    add class HibernateUtil

--- src/main/java/com/mycompany/crud/hibernate/HibernateUtil.java
+++ src/main/java/com/mycompany/crud/hibernate/HibernateUtil.java
@@ -4,10 +4,29 @@
  */
 package com.mycompany.crud.hibernate;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 //agregar la funcionalidad de hibernate util
 /**
- *
+ * Wendy Vallejo
  */
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+    private HibernateUtil(){     
+    }
+    static{
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
